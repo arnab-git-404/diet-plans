4eb19bee508a337bc702591575610c62ed3b8c61
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-11-24 12:44:47 +0530

    library

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export interface DietPlan {
+    _id?: string;
+    title: string;
+    description: string;
+    imageUrl: string;
+    pdfUrl: string;
+    price: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface Submission {
+    _id?: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    email: string;
+    age: string;
+    gender: string;
+    weight: string;
+    healthGoal: string;
+    healthGoalOther?: string;
+    dietPlanId: string;
+    dietPlanTitle: string;
+    submittedAt: Date;
+}
+
+export interface AdminUser {
+    email: string;
+    password: string;
+}
