e89db1176ae5969b1f55cb8edcaa5b41b71af6cc
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-11-26 23:30:25 +0530

    font package installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,78 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
+import {
+  Montserrat,
+  Open_Sans,
+  Playfair_Display,
+  Raleway,
+  Poppins,
+  Lora,
+  Roboto,
+  Lato,
+  Merriweather,
+  Source_Sans_3,
+} from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-openSans",
+});
+
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-playfair",
+});
+
+export const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-raleway",
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-lora",
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+});
+
+export const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  variable: "--font-lato",
+});
+
+export const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  variable: "--font-merriweather",
+});
+
+export const sourceSans = Source_Sans_3({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700", "800", "900"],
+  variable: "--font-sourceSans",
+});
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,7 +97,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
+      <body
+        className={`${inter.variable} ${montserrat.variable} ${openSans.variable} 
+                 ${playfair.variable} ${raleway.variable} 
+                 ${poppins.variable} ${lora.variable}
+                 ${roboto.variable} ${lato.variable}
+                 ${merriweather.variable} ${sourceSans.variable} antialiased`}
+      >
         <div className="min-h-screen flex flex-col">
           <Navbar />
           <main className="flex-1">
