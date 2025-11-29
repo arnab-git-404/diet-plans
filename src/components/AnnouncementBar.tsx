7fcba020f78d42998e78f1dff8590afb78ec4d29
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-11-29 14:45:15 +0530

    Update AnnouncementBar.tsx

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -2,19 +2,47 @@
 
 export default function AnnouncementBar() {
   return (
-    <div className=" h-[33px] announcement-bar bg-white text-[#0b4c49] py-3">
-      <div className="announcement-bar--inner overflow-hidden relative">
-        <div className=" h-[13px] flex animate-marquee whitespace-nowrap">
-          <div className="announcement-bar--item inline-block px-8">
+    <div className="h-[33px] announcement-bar bg-white text-[#0b4c49] overflow-hidden group">
+      <div className="announcement-bar--inner relative h-full flex items-center">
+        <div className="flex animate-marquee whitespace-nowrap group-hover:pause">
+          <div 
+            className="announcement-bar--item inline-block px-8 font-normal text-[13px]" 
+            style={{ 
+              fontFamily: '"Open Sans", sans-serif',
+              letterSpacing: '0.39px',
+              lineHeight: '13px'
+            }}
+          >
             FSSAI Approved | ISO 22000 certified | GMP certified | HACCP certified | NABL Tested
           </div>
-          <div className="announcement-bar--item inline-block px-8">
+          <div 
+            className="announcement-bar--item inline-block px-8 font-normal text-[13px]" 
+            style={{ 
+              fontFamily: '"Open Sans", sans-serif',
+              letterSpacing: '0.39px',
+              lineHeight: '13px'
+            }}
+          >
             FSSAI Approved | ISO 22000 certified | GMP certified | HACCP certified | NABL Tested
           </div>
-          <div className="announcement-bar--item inline-block px-8">
+          <div 
+            className="announcement-bar--item inline-block px-8 font-normal text-[13px]" 
+            style={{ 
+              fontFamily: '"Open Sans", sans-serif',
+              letterSpacing: '0.39px',
+              lineHeight: '13px'
+            }}
+          >
             FSSAI Approved | ISO 22000 certified | GMP certified | HACCP certified | NABL Tested
           </div>
-          <div className="announcement-bar--item inline-block px-8">
+          <div 
+            className="announcement-bar--item inline-block px-8 font-normal text-[13px]" 
+            style={{ 
+              fontFamily: '"Open Sans", sans-serif',
+              letterSpacing: '0.39px',
+              lineHeight: '13px'
+            }}
+          >
             FSSAI Approved | ISO 22000 certified | GMP certified | HACCP certified | NABL Tested
           </div>
         </div>
